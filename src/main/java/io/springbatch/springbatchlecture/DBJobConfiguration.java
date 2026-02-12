aad581189a1dadbf649eaa36f912ccf7168621ba
jongho <tlem2@naver.com>
2026-02-12 15:52:47 +0900

    Spring batch meta data 스키마 설정 및 yml 설정

---
+++ src/main/java/io/springbatch/springbatchlecture/DBJobConfiguration.java
@@ -0,0 +1,57 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class DBJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job dbTestJob() {
+        return jobBuilderFactory.get("DB-Test-Job")
+                .start(dbTestStep1())
+                .next(dbTestStep2())
+                .build();
+    }
+
+    @Bean
+    public Step dbTestStep1() {
+        return stepBuilderFactory.get("dbTestStep1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        log.info("DB TEST STEP 1");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step dbTestStep2() {
+        return stepBuilderFactory.get("dbTestStep2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        log.info("DB TEST STEP 2");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}
