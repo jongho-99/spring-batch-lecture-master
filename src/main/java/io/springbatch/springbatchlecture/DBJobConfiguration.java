de2101f7c86f2f790f66da5edf95ca6f8381dfed
jongho <tlem2@naver.com>
2026-02-13 10:02:25 +0900

    숫자 2 추가2

--- src/main/java/io/springbatch/springbatchlecture/DBJobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/DBJobConfiguration.java
@@ -35,7 +35,7 @@ public class DBJobConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        log.info("DB TEST STEP 1");
+                        log.info("DB TEST STEP 122222");
                         return RepeatStatus.FINISHED;
                     }
                 })
