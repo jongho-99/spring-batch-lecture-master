6cc866a1b9963afe2f9839289f157a33a3e36ceb
jongho <tlem2@naver.com>
2026-02-19 09:11:49 +0900

    20260219 오전 커밋

--- src/main/java/io/springbatch/springbatchlecture/JobInstanceConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobInstanceConfiguration.java
@@ -1,7 +1,7 @@
 package io.springbatch.springbatchlecture;
 
+
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -15,43 +15,42 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-@Slf4j
-public class DBJobConfiguration {
+public class JobInstanceConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job dbTestJob() {
-        return jobBuilderFactory.get("DB-Test-Job")
-                .start(dbTestStep1())
-                .next(dbTestStep2())
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .next(step2())
                 .build();
     }
 
     @Bean
-    public Step dbTestStep1() {
-        return stepBuilderFactory.get("dbTestStep1")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        log.info("DB TEST STEP 1");
-                        return RepeatStatus.FINISHED;
+                        return null;
                     }
                 })
                 .build();
     }
 
     @Bean
-    public Step dbTestStep2() {
-        return stepBuilderFactory.get("dbTestStep2")
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        log.info("DB TEST STEP 2");
-                        return RepeatStatus.FINISHED;
+                        return null;
                     }
                 })
                 .build();
     }
+
+
 }
